e2b15b493807ca120e71a2b12dca2a5e8ea152b2
kalashovkerim <kkalashov.unity@gmail.com>
2023-03-04 03:13:23 +0400

    Todolist 60%

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoForm from "./Components/TodoForm";
-
+import JsonData from "./info.json"
 function App() {
   return (
-    <TodoForm/>
+      <div className="container">
+        <TodoForm/>
+      </div>
   );
 }
 
--- todolist/src/Components/TodoForm.jsx
+++ todolist/src/Components/TodoForm.jsx
@@ -1,30 +1,73 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
+import '../Styles/TodoForm.css'
+//import JsonData from '../info.json'
 function TodoForm() {
+    const [id, setId] = useState("1");
+    const [task, setTask] = useState("This is task");
+    const [description, setDesc] = useState("This is desc");
+    const [done, setDone] = useState(false);
+
+    // save data to localStorage
+    const saveStateToLocalStorage = () => {
+        localStorage.setItem('state', JSON.stringify({id, task, description, done }));
+    }
+    function getData() {
+        let data = localStorage.getItem('state');
+        return data ? JSON.parse(data) : [];
+    }
+
+    // Fetch data from local storage
+    const getStateFromLocalStorage = () => {
+        let data = getData();
+        if(data !== undefined) {
+            setId(data.id)
+            setTask(data.task);
+            setDesc(data.description);
+            setDone(data.done);
+        }
+    }
+
+    useEffect(() => {
+        // Fetch data from local storage
+        getStateFromLocalStorage();
+    }, []);
+
+    const DisplayData = () => {
+        const jsonData = getData();
+        const data = jsonData ? JSON.parse(jsonData) : [];
+        return (
+            <tbody>
+            {data.map((info) => (
+                <tr key={info.id}>
+                    <td>{info.task}</td>
+                    <td>{info.description}</td>
+                    <td><input type="checkbox" defaultChecked={info.done}/></td>
+                </tr>
+            ))}
+            </tbody>
+        );
+    }
     return (
-        <div>
-            <input placeholder="Enter the task"/>
-            <button>Add</button>
-            <table>
-                <thead>
-                    <th>Task</th>
-                    <th>Description</th>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>Run</td>
-                    <td>Run 300 km</td>
-                </tr>
-                <tr>
-                    <td>Run</td>
-                    <td>Run 300 km</td>
-                </tr>
-                <tr>
-                    <td>Run</td>
-                    <td>Run 300 km</td>
-                </tr>
-                </tbody>
-            </table>
+        <div className="bg">
+            <div className="main">
+                <div className="form">
+                    <input placeholder="Enter the task"/>
+                    <input placeholder="Enter the description"/>
+                    <button onClick={saveStateToLocalStorage}>Add</button>
+                </div>
+                <div className="table-wrapper">
+                    <table className="fl-table">
+                        <thead>
+                        <tr>
+                            <th>Task</th>
+                            <th>Description</th>
+                            <th>Done</th>
+                        </tr>
+                        </thead>
+                           {DisplayData()}
+                    </table>
+                </div>
+            </div>
         </div>
     );
 }
