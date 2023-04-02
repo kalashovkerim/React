b2658fc258793a048b23e9d211393df633c7455a
kalashovkerim <kkalashov.unity@gmail.com>
2023-04-02 15:47:17 +0400

    Movie searcher

--- movie-search/src/App.js
+++ movie-search/src/App.js
@@ -1,11 +1,51 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import "./App.css";
+
+const API_KEY = "8c990900";
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (query) => {
+    const url = `http://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.Search) {
+      setMovies(data.Search);
+    }
+  };
+
+  useEffect(() => {
+    searchMovies(query);
+  }, [query]);
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  };
+
   return (
-    <div className="App">
-
-    </div>
+      <div>
+        <div className="search-container">
+          <input
+              type="text"
+              value={query}
+              onChange={handleChange}
+              className="search-input"
+              placeholder="Search for movies..."
+          />
+        </div>
+        <ul className="movies-list">
+          {movies.map((movie) => (
+              <li key={movie.imdbID} className="movie-item">
+                {movie.Title}
+              </li>
+          ))}
+        </ul>
+      </div>
   );
-}
+};
 
 export default App;
+
