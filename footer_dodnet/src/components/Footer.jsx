b3e71c0f6fc6766cddaa90a218de0c1272e87c4f
kalashovkerim <kkalashov.unity@gmail.com>
2023-04-09 15:44:00 +0400

    footer

---
+++ footer_dodnet/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from "../Styles/footer.module.css";
+
+function Footer(props) {
+    return (
+        <div className={classes.myfooter}>
+            hi
+        </div>
+    );
+}
+
+export default Footer;
